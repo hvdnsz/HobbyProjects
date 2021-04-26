7afeb8cc4020bbde2ebfd9f8c4786971718c72c7
hvdnsz <hamori.marton.hu@gmail.com>
2021-04-26 20:32:35 +0200

    Initial commit of the numpy implementation of the four in a row game.

---
+++ four_in_a_row_numpy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+Python version 3.9 and above. (not use typing library)
+Four in a row terminal/console implementation based on numpy.
+Windows: pip install numpy
+Linux: pip3 install numpy
+"""
+
+# IMPORTS
+import numpy
+
+
+# this class handle the gameboard
+class GameBoard:
+    """A numpy-based gameboard\n"""
+    pass
+
+
+# MAIN
+def main():
+    """The so called main-fuction.\n"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
