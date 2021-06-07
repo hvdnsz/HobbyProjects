83785728716435992e3faf421f064d34e3f25b60
hvdnsz <hamori.marton.hu@gmail.com>
2021-06-07 17:40:39 +0200

    Technically this is alpha

--- visual_game.py
+++ visual_game.py
@@ -22,6 +22,7 @@ BLACK = Color('black')
 RED = Color('red')
 BLUE = Color('blue')
 YELLOW = Color('yellow')
+PURPLE = Color('purple')
 
 # dimensions of the board
 ROW = 6
@@ -96,7 +97,14 @@ class MainWindow:
         # init pygame
         pygame.init()
 
+        # init basic font system
+        self.my_font = pygame.font.Font(None, 128)
+
+        # for switching between two players
         self.actual_p = player_switcher(1, 2)
+
+        # duck tape for game over
+        self.is_game_over = False
 
         # init the screen
         pygame.display.set_caption('Four in a row :)')
@@ -105,47 +113,69 @@ class MainWindow:
         # "static" part of the game
         self.gameboard = IAmTheVisualGameBoard(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+    def game_over(self, winner_player):
+        pass
+
     def main_loop(self):
         done = False
         # main loop
         while not done:
-            # main event loop
-            # only for exit
+
+            # event loop to clear the event queue
             for event in pygame.event.get():
+                # exit by pressing the red button
                 if event.type == pygame.QUIT:
                     done = True
+                # exit by pressing escape key
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         done = True
 
-                # handle mouse left button
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # handle the left mouse button
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and \
+                        not self.is_game_over:
                     # get the x value of the cursor
                     mouse_pos_x = pygame.mouse.get_pos()[0]
+                    # determine which column the cursor is in
                     column = mouse_pos_x // TILE_SIZE
 
                     # keres egy szabad sort
                     szabad_sor = self.gameboard.search_row(column)
 
-                    # ha van kicseréli
+                    # there is a free row (the move is valid)
                     if szabad_sor != -1:
 
+                        # switch player
                         player = next(self.actual_p)
+
+                        # place the disk
                         self.gameboard.matrix[szabad_sor][column] = player
 
-                        # megnézi hogy nyert-e a player
+                        # check if it was a winning move
                         win = self.gameboard.check_for_winning(szabad_sor, column, player)
-                        if win:
-                            print(f'Player {player} has won!')
 
-            # draw
+                        # if it was "execute" game over process
+                        if win:
+                            self.is_game_over = True
+
+            # clear the screen with rgb black
             self.screen.fill((0, 0, 0))
 
             # redraw the gameboard
             self.gameboard.draw()
-
             self.screen.blit(self.gameboard.image, (0, 0))
 
+            # drawing the game over text
+            # over the game board
+            if self.is_game_over:
+                # TODO: I am really sorry, please fix me soon :(
+                # line 134 and __init__
+
+                next(self.actual_p)
+                game_over_text = self.my_font.render('Game Over!', True, PURPLE)
+                self.screen.blit(game_over_text, (TILE_SIZE, TILE_SIZE))
+
+            # update the whole screen
             pygame.display.update()
 
         pygame.quit()
