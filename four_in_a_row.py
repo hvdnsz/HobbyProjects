36bf4b498a634967550e5dd037daf83bd08608c4
hvdnsz <74798660+hvdnsz@users.noreply.github.com>
2021-08-08 03:51:45 +0200

    restore lost files
    issues with git

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,210 +1,223 @@
 #!/usr/bin/python
-"""
-The classic game of Connect Four is played by two players in a grid of 6 row_count
-by 7 column_count, standing vertically. Players alternately drop one token from
-the top of one column, and the token falls to the lowest possible grid cell,
-stacking up on the tokens below it. The first player to make a horizontal,
-vertical or diagonal line of 4 tokens wins the game.
 
-From the description of one game grid, your program must determine in which
-column_count each player may complete a line if they play first on the next turn.
-"""
-from enum import Enum
+#########
+# PLANS #
+#########
+# TODO: SELECT COLUMN SUPER MARIO 2 DESIGN PICK UP AND PUT ON THE TOP OF HEAD
 
 
-# UTILS
-def player_switcher(p1, p2):
-    while True:
-        yield p1
-        yield p2
+###########
+# IMPORTS #
+###########
+import sys
+import pygame
+from pygame import Rect, Surface, Color
+from pygame.font import Font
+from four_in_a_row import Connect4GameBoard, player_switcher
 
 
-def force_within_range(min_: int, max_: int, prompt: str) -> int:
-    while True:
-        out = int(input(prompt))
-        if min_ <= out <= max_:
-            return out
+#############
+# CONSTANTS #
+#############
+# colors
+BLACK = Color('black')
+RED = Color(150, 0, 0)
+BLUE = Color(0, 0, 150)
+YELLOW = Color(150, 150, 0)
+GREEN = Color(0, 150, 0)
 
-        print('This option is invalid...try again!')
+# dimensions of the board
+ROW = 6
+COLUMN = 7
 
+# dimensions of the tiles
+TILE_SIZE = 100
+TILE_PAD = 12
 
-class FieldMark(Enum):
-    EMPTY = 0
-    PLAYER_1 = 1
-    PLAYER_2 = 2
-
-
-class Connect4GameBoard(object):
-    # CONSTANTS
-    EMPTY_FIELD = 0
-    PLAYER_ONE = 1
-    PLAYER_TWO = 2
-
-    def __init__(self, row=6, column=7) -> None:
-        # dimensions
-        self.row_count: int = row
-        self.column_count: int = column
-
-        self.disks_played: int = 0
-        self.disks_limit: int = row * column
-
-        # this is the structure of the game board
-        self.matrix: list[list] = [[self.EMPTY_FIELD for _ in range(column)] for _ in range(row)]
-
-    def search_row(self, col: int) -> int:
-        """
-        Find the index of the lowest free row (if there is one).
-        :param col: index of the column
-        :type col: int
-        :return: the index of the first free row or None
-        """
-
-        # now the user can give float like this (2.0)
-        col = int(col)
-
-        for i, row in enumerate(self.matrix):
-            if row[col] == self.EMPTY_FIELD:
-                return i
-        else:
-            return -1
-
-    def check_for_winning(self, row: int, column: int, player) -> bool:  # this is zero-based
-        """Calculate the minimal free space around a disk/coordination and check winning."""
-        # todo: potential performance issue
-        border_x: int = self.column_count - 1
-        border_y: int = self.row_count - 1
-
-        # horizontal todo mennyi a különbség comma potential helper function
-        sub_x = column if column < 3 else 3
-        add_x = border_x - column if column > border_x - 3 else 3
-        # vertical
-        sub_y = row if row < 3 else 3
-        add_y = border_y - row if row > border_y - 3 else 3
-
-        def check_horizontal_line() -> bool:
-            matches = 0
-            for x in range(column - sub_x, column + add_x + 1):
-
-                if self.matrix[row][x] == player:
-                    matches += 1
-                    if matches == 4:
-                        return True
-
-                elif matches > 0:
-                    matches = 0
-
-            return False
-
-        def check_vertical_line() -> bool:
-            matches = 0
-            for y in range(row - sub_y, row):
-                if self.matrix[y][column] != player:
-                    return False
-                matches += 1
-
-            # do not check the current disk
-            return True if matches == 3 else False
-
-        def check_lb_rt_diagonal_line() -> bool:
-            matches = 0
-            left_corner = min(sub_x, sub_y)
-            right_corner = min(add_x, add_y)
-
-            for x, y in zip(range(column - left_corner, column + right_corner + 1),
-                            range(row - left_corner, row + right_corner + 1)):
-                if self.matrix[y][x] == player:
-                    matches += 1
-                    if matches == 4:
-                        return True
-
-                elif matches > 0:
-                    matches = 0
-
-            return False
-
-        def check_lt_rb_diagonal_line() -> bool:
-            matches = 0
-            left_corner = min(sub_x, add_y)
-            right_corner = min(add_x, sub_y)
-
-            for x, y in zip(range(column - left_corner, column + right_corner + 1),
-                            range(row + left_corner, row - right_corner - 1, -1)):  # y is decreasing
-                if self.matrix[y][x] == player:
-                    matches += 1
-                    if matches == 4:
-                        return True
-
-                elif matches > 0:
-                    matches = 0
-
-            return False
-
-        for func in (check_horizontal_line, check_vertical_line, check_lt_rb_diagonal_line, check_lb_rt_diagonal_line):
-            if func() is True:
-                return True
-
-        return False
+# dimensions of the screen
+SCREEN_WIDTH = COLUMN * TILE_SIZE
+SCREEN_HEIGHT = ROW * TILE_SIZE
 
 
-class Connect4Game(Connect4GameBoard):
-
-    def __init__(self, player1=None, player2=None):
-        super(Connect4Game, self).__init__()
-        if player1 is not None:
-            self.PLAYER_ONE = player1
-        if player2 is not None:
-            self.PLAYER_TWO = player2
-
-    def print_matrix(self) -> None:
-        """Just the print the matrix human readable."""
-        print('#' * 13)
-        for sor in self.matrix[::-1]:
-            print(' '.join(map(str, sor)))
-        print('#' * 13)
-
-    def play_game(self):
-        print('The game has begun!!!')
-
-        result_of_the_game = None
-
-        for actual_player in player_switcher(self.PLAYER_ONE, self.PLAYER_TWO):
-
-            # the game board is full
-            if self.disks_played > self.disks_limit:
-                result_of_the_game = 'draw'
-                break
-
-            # inform the player about the actual state
-            self.print_matrix()
-            print('It is the turn of player -', actual_player)
-
-            # get a valid column
-            column: int = int(input('pleas enter the number: ')) - 1
-            # get the row
-            for i, row in enumerate(self.matrix):
-                if row[column] == self.EMPTY_FIELD:
-                    row = i
-                    break
-            else:
-                row = 0
-
-            # place/drop a disk from the actual player
-            self.matrix[row][column] = actual_player
-
-            # check winning
-            if self.check_for_winning(row, column, actual_player):
-                result_of_the_game = str(actual_player) + ' win!!!'
-                break
-
-        self.print_matrix()
-        print('the result is:', result_of_the_game)
+#######################
+# ASSEMBLE GAME TILES #
+#######################
+def draw_this(color, width, pad) -> Surface:
+    image = pygame.Surface((width, width))
+    radius = width // 2
+    image.fill(BLUE)
+    pygame.draw.circle(image, color, (radius, radius), radius - pad)
+    return image
 
 
-def main():
-    game = Connect4Game()
-    game.play_game()
+TILE_RED: Surface = draw_this(RED, TILE_SIZE, TILE_PAD)
+TILE_YELLOW: Surface = draw_this(YELLOW, TILE_SIZE, TILE_PAD)
+TILE_EMPTY: Surface = draw_this(BLACK, TILE_SIZE, TILE_PAD)
+
+
+#####################################################
+# VISUAL GAME BOARD DISPLAYING THE ACTUAL GAME STATE #
+#####################################################
+class GameBoardUI(Connect4GameBoard):
+
+    TILEMAP: dict[int, Surface] = {
+        0: TILE_EMPTY,
+        1: TILE_RED,
+        2: TILE_YELLOW
+    }
+
+    def __init__(self, surface_rect: Rect, dimensions: tuple[int, int], tile_size: int) -> None:
+
+        # init the inherited part of the class
+        super(GameBoardUI, self).__init__(*dimensions)
+
+        self.image: Surface = pygame.Surface((surface_rect.width, surface_rect.height))
+        self.rect: Rect = surface_rect
+
+        self.tile_size: int = tile_size
+
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.blit(self.image, self.rect)
+
+    def draw_all_tiles(self) -> None:
+        """Draw all tiles to local image surface."""
+        y: int = self.rect.bottom - self.tile_size
+        for row in self.matrix:
+            x: int = 0
+            for tile_id in row:
+                self.image.blit(self.TILEMAP[tile_id], (x, y))
+                x += self.tile_size
+            y -= self.tile_size
+
+
+class GameOver:
+    def __init__(self, pos: tuple[int, int], color: Color, size: int):
+        # default color
+        self.default_color: Color = color
+
+        # just for safety
+        pygame.font.init()
+        # init font
+        self.my_font: Font = pygame.font.Font(None, size)
+
+        self.image: Surface = self.my_font.render('Default Text', True, self.default_color)
+        # location of the text
+        self.rect: Rect = self.image.get_rect(topleft=pos)
+
+    def set_text(self, text: str, color=None):
+        # optional parameter
+        if color is None:
+            color = self.default_color
+        # re-render text to image surface
+        self.image = self.my_font.render(text, True, color)
+
+    def draw(self, surface: pygame.Surface):
+        surface.blit(self.image, self.rect)
+
+
+#############################
+# HANDLE THE DISPLAY SCREEN #
+#############################
+class MainWindow:
+
+    def __init__(self):
+        # init pygame
+        pygame.init()
+
+        # for switching between two players
+        self.actual_p = player_switcher(1, 2)
+
+        # init game over text
+        # hardcoded
+        self.game_over_screen = GameOver((TILE_SIZE + TILE_SIZE // 5, TILE_SIZE), GREEN, 80)
+
+        # init display screen
+        pygame.display.set_caption('Four in a row :)')
+        flags: int = 0
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags)
+
+        # "static" part of the game
+        self.game_board = GameBoardUI(Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), (6, 7), TILE_SIZE)
+
+    def game_over(self, winner_player):
+        self.game_over_screen.set_text(f"Player {winner_player} has won!")
+
+    def main_loop(self):
+        done: bool = False
+        game_over: bool = False
+        do_render: bool = True
+        while not done:
+            # clear event queue
+            for event in pygame.event.get():
+                # exit by pressing the red button
+                if event.type == pygame.QUIT:
+                    done = True
+                # exit by pressing escape key
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        done = True
+                    if event.key == pygame.K_r:
+                        self.game_board.reset()
+                        do_render = True
+                        game_over = False
+
+                elif not game_over and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # get the x value of the cursor
+                    mouse_pos_x = event.pos[0]
+                    # determine which column the cursor is in
+                    column = mouse_pos_x // TILE_SIZE
+
+                    # find the first free slot/row
+                    free_row = self.game_board.search_row(column)
+
+                    # there is a free row (the move is valid)
+                    if free_row != -1:
+                        # turn on render for this frame
+                        do_render = True
+
+                        # switch player
+                        player = next(self.actual_p)
+
+                        # place the disk
+                        self.game_board.matrix[free_row][column] = player
+
+                        # check if it was a winning move
+                        win = self.game_board.check_for_winning(free_row, column, player)
+
+                        # if it was "execute" game over process
+                        if win:
+                            self.game_over(player)
+                            game_over = True
+
+            # clear the screen with rgb black
+            self.screen.fill((0, 0, 0))
+
+            # redraw the game board
+            self.game_board.draw_all_tiles()
+
+            # blit to display
+            self.game_board.draw(self.screen)
+
+            # blit game over text
+            if game_over:
+                self.game_over_screen.draw(self.screen)
+
+            # update the whole screen
+            if do_render:
+                do_render = False
+                pygame.display.update()
+
+        pygame.quit()
+
+
+########
+# MAIN #
+########
+def main() -> None:
+    game = MainWindow()
+    game.main_loop()
+    sys.exit()
 
 
 if __name__ == '__main__':
     main()
-    exit()
