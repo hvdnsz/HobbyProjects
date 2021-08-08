8627d9a5bf26ea0d77760d1230c3f855b4162f76
hvdnsz <74798660+hvdnsz@users.noreply.github.com>
2021-08-08 03:52:58 +0200

    restore lost files
    issues with git

--- visual_game.py
+++ visual_game.py
@@ -9,8 +9,10 @@
 ###########
 # IMPORTS #
 ###########
+import sys
 import pygame
-from pygame import Color
+from pygame import Rect, Surface, Color
+from pygame.font import Font
 from four_in_a_row import Connect4GameBoard, player_switcher
 
 
@@ -19,10 +21,10 @@ from four_in_a_row import Connect4GameBoard, player_switcher
 #############
 # colors
 BLACK = Color('black')
-RED = Color('red')
-BLUE = Color('blue')
-YELLOW = Color('yellow')
-PURPLE = Color('purple')
+RED = Color(150, 0, 0)
+BLUE = Color(0, 0, 150)
+YELLOW = Color(150, 150, 0)
+GREEN = Color(0, 150, 0)
 
 # dimensions of the board
 ROW = 6
@@ -40,52 +42,77 @@ SCREEN_HEIGHT = ROW * TILE_SIZE
 #######################
 # ASSEMBLE GAME TILES #
 #######################
-def draw_this(color, width, pad) -> pygame.Surface:
+def draw_this(color, width, pad) -> Surface:
     image = pygame.Surface((width, width))
-
     radius = width // 2
-
     image.fill(BLUE)
     pygame.draw.circle(image, color, (radius, radius), radius - pad)
-
     return image
 
 
-TILE_RED = draw_this(RED, TILE_SIZE, TILE_PAD)
-TILE_YELLOW = draw_this(YELLOW, TILE_SIZE, TILE_PAD)
-TILE_EMPTY = draw_this(BLACK, TILE_SIZE, TILE_PAD)
+TILE_RED: Surface = draw_this(RED, TILE_SIZE, TILE_PAD)
+TILE_YELLOW: Surface = draw_this(YELLOW, TILE_SIZE, TILE_PAD)
+TILE_EMPTY: Surface = draw_this(BLACK, TILE_SIZE, TILE_PAD)
 
 
 #####################################################
-# VISUAL GAMEBOARD DISPLAYING THE ACTUAL GAME STATE #
+# VISUAL GAME BOARD DISPLAYING THE ACTUAL GAME STATE #
 #####################################################
-class IAmTheVisualGameBoard(Connect4GameBoard):
+class GameBoardUI(Connect4GameBoard):
 
-    cuclik = {
+    TILEMAP: dict[int, Surface] = {
         0: TILE_EMPTY,
         1: TILE_RED,
         2: TILE_YELLOW
     }
 
-    def __init__(self, width: int, height: int):
-        super(IAmTheVisualGameBoard, self).__init__()
+    def __init__(self, surface_rect: Rect, dimensions: tuple[int, int], tile_size: int) -> None:
 
-        self.image = pygame.Surface((width, height))
-        self.image.fill((250, 250, 250))
+        # init the inherited part of the class
+        super(GameBoardUI, self).__init__(*dimensions)
 
-        self.part_width = width
-        self.part_height = height
+        self.image: Surface = pygame.Surface((surface_rect.width, surface_rect.height))
+        self.rect: Rect = surface_rect
 
-    def draw(self):
-        pos_y = self.part_height
+        self.tile_size: int = tile_size
 
-        # draw every tile depend on its id number
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.blit(self.image, self.rect)
+
+    def draw_all_tiles(self) -> None:
+        """Draw all tiles to local image surface."""
+        y: int = self.rect.bottom - self.tile_size
         for row in self.matrix:
-            pos_y -= TILE_SIZE
-            pos_x = 0
-            for col in row:
-                self.image.blit(self.cuclik[col], (pos_x, pos_y))
-                pos_x += TILE_SIZE
+            x: int = 0
+            for tile_id in row:
+                self.image.blit(self.TILEMAP[tile_id], (x, y))
+                x += self.tile_size
+            y -= self.tile_size
+
+
+class GameOver:
+    def __init__(self, pos: tuple[int, int], color: Color, size: int):
+        # default color
+        self.default_color: Color = color
+
+        # just for safety
+        pygame.font.init()
+        # init font
+        self.my_font: Font = pygame.font.Font(None, size)
+
+        self.image: Surface = self.my_font.render('Default Text', True, self.default_color)
+        # location of the text
+        self.rect: Rect = self.image.get_rect(topleft=pos)
+
+    def set_text(self, text: str, color=None):
+        # optional parameter
+        if color is None:
+            color = self.default_color
+        # re-render text to image surface
+        self.image = self.my_font.render(text, True, color)
+
+    def draw(self, surface: pygame.Surface):
+        surface.blit(self.image, self.rect)
 
 
 #############################
@@ -97,31 +124,30 @@ class MainWindow:
         # init pygame
         pygame.init()
 
-        # init basic font system
-        self.my_font = pygame.font.Font(None, 128)
-
         # for switching between two players
         self.actual_p = player_switcher(1, 2)
 
-        # duck tape for game over
-        self.is_game_over = False
+        # init game over text
+        # hardcoded
+        self.game_over_screen = GameOver((TILE_SIZE + TILE_SIZE // 5, TILE_SIZE), GREEN, 80)
 
-        # init the screen
+        # init display screen
         pygame.display.set_caption('Four in a row :)')
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        flags: int = 0
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags)
 
         # "static" part of the game
-        self.gameboard = IAmTheVisualGameBoard(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.game_board = GameBoardUI(Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), (6, 7), TILE_SIZE)
 
     def game_over(self, winner_player):
-        pass
+        self.game_over_screen.set_text(f"Player {winner_player} has won!")
 
     def main_loop(self):
-        done = False
-        # main loop
+        done: bool = False
+        game_over: bool = False
+        do_render: bool = True
         while not done:
-
-            # event loop to clear the event queue
+            # clear event queue
             for event in pygame.event.get():
                 # exit by pressing the red button
                 if event.type == pygame.QUIT:
@@ -130,53 +156,56 @@ class MainWindow:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         done = True
+                    if event.key == pygame.K_r:
+                        self.game_board.reset()
+                        do_render = True
+                        game_over = False
 
-                # handle the left mouse button
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and \
-                        not self.is_game_over:
+                elif not game_over and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     # get the x value of the cursor
-                    mouse_pos_x = pygame.mouse.get_pos()[0]
+                    mouse_pos_x = event.pos[0]
                     # determine which column the cursor is in
                     column = mouse_pos_x // TILE_SIZE
 
-                    # keres egy szabad sort
-                    szabad_sor = self.gameboard.search_row(column)
+                    # find the first free slot/row
+                    free_row = self.game_board.search_row(column)
 
                     # there is a free row (the move is valid)
-                    if szabad_sor != -1:
+                    if free_row != -1:
+                        # turn on render for this frame
+                        do_render = True
 
                         # switch player
                         player = next(self.actual_p)
 
                         # place the disk
-                        self.gameboard.matrix[szabad_sor][column] = player
+                        self.game_board.matrix[free_row][column] = player
 
                         # check if it was a winning move
-                        win = self.gameboard.check_for_winning(szabad_sor, column, player)
+                        win = self.game_board.check_for_winning(free_row, column, player)
 
                         # if it was "execute" game over process
                         if win:
-                            self.is_game_over = True
+                            self.game_over(player)
+                            game_over = True
 
             # clear the screen with rgb black
             self.screen.fill((0, 0, 0))
 
-            # redraw the gameboard
-            self.gameboard.draw()
-            self.screen.blit(self.gameboard.image, (0, 0))
+            # redraw the game board
+            self.game_board.draw_all_tiles()
 
-            # drawing the game over text
-            # over the game board
-            if self.is_game_over:
-                # TODO: I am really sorry, please fix me soon :(
-                # line 134 and __init__
+            # blit to display
+            self.game_board.draw(self.screen)
 
-                next(self.actual_p)
-                game_over_text = self.my_font.render('Game Over!', True, PURPLE)
-                self.screen.blit(game_over_text, (TILE_SIZE, TILE_SIZE))
+            # blit game over text
+            if game_over:
+                self.game_over_screen.draw(self.screen)
 
             # update the whole screen
-            pygame.display.update()
+            if do_render:
+                do_render = False
+                pygame.display.update()
 
         pygame.quit()
 
@@ -184,5 +213,11 @@ class MainWindow:
 ########
 # MAIN #
 ########
-game = MainWindow()
-game.main_loop()
+def main() -> None:
+    game = MainWindow()
+    game.main_loop()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
